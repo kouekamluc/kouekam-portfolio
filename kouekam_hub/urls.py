4375b58162799e70d6bee6bd07919f9b62393fc9
kouekam <kouekamkamgouluc@gmail.com>
2025-12-10 10:01:10 +0100

    Add CSS test page for debugging
    - Add css_test.html template with diagnostic tests
    - Add /css-test/ URL route
    - Helps verify if CSS is loading and applying correctly

--- kouekam_hub/urls.py
+++ kouekam_hub/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("css-test/", TemplateView.as_view(template_name="css_test.html"), name="css_test"),
     path("", include("portfolio.urls")),
     path("academic/", include("academic.urls")),
     path("productivity/", include("productivity.urls")),
