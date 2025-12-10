2b45b2a2fa8b05cadf6ede2c32d8bbe4363ea6d5
kouekam <kouekamkamgouluc@gmail.com>
2025-12-10 15:28:00 +0100

    Add missing context_processors.py file for profile context

---
+++ kouekam_hub/context_processors.py
@@ -0,0 +1,9 @@
+from portfolio.models import Profile
+
+def profile_context(request):
+    """Make profile available in all templates"""
+    profile = Profile.objects.first()
+    return {
+        'profile': profile,
+    }
+
