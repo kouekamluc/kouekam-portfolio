ddb7cde3d16fbafcc1babd3fa07657a0a1385819
kouekam <kouekamkamgouluc@gmail.com>
2026-01-11 13:38:09 +0100

    fix: reorder blog URLs to fix 404 on blog create page
    Move 'post/create/' pattern before 'post/<slug:slug>/' pattern to prevent
    Django from matching 'create' as a slug parameter

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('post/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('post/create/', views.blog_create, name='blog_create'),
+    path('post/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('post/<slug:slug>/update/', views.blog_update, name='blog_update'),
     path('post/<slug:slug>/delete/', views.blog_delete, name='blog_delete'),
     path('tutorials/', views.tutorial_list, name='tutorial_list'),
